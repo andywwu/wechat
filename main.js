e579c924a855d6a20b62badb9b97aa4615abd6e0
andywu <andy_wux@163.com>
2017-08-30 18:16:58 +0800

    8-30

--- main.js
+++ main.js
@@ -1,3 +1,38 @@
 define(function(){
+	var doc = $(document);
+	console.log(111);
+	var App = {
+		init: function() {
+			this.initStatus();
+			this.render();
+			this.bindEvent();		
+		},
+		initStatus: function() {
+			$('.grid-item').append('<span class="edit-name" onClick="editName(this)"><i class="iconfont">&#xe604;</i></span>');
+			var confirm1 = new JDUI.instance.Toast({
+			    hook: '.confirm-twobtn-wrap',
+			    type: 'two',
+			    teplMap: {
+			        title: '修改名称',
+			        content: '<p>可修改自定义名称</p>'
+			    },
+			    letBtnFn: function(e) {
+			        confirm1.closeConfirm();
+			    },
+			    rightBtnFn: function(e) {
+			        confirm1.closeConfirm();
+			    }
+			});
+		},
+		render: function() {
 
+		},
+		bindEvent: function() {
+
+		},
+	};
+	function editName(bg) {
+
+	}
+	return App;
 });
