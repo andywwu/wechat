b92538e1bb1bf1499f17bf31c6bb03dde0ae4ced
andywu <andy_wux@163.com>
2017-08-31 18:13:25 +0800

    8-31

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-define(function(){
 	var doc = $(document);
 	console.log(111);
+	var modeName = '';
 	var App = {
 		init: function() {
 			this.initStatus();
@@ -8,15 +8,109 @@ define(function(){
 			this.bindEvent();		
 		},
 		initStatus: function() {
+			var power = new JDUI.instance.SwitchCell({
+		        title: '电源',
+		        type: JDUI.type.SwitchCell.switch, //JD、jd标准样式、 Ali、阿里标准样式
+		        value: '1', //和正常一样，开1，关0
+		        hook: '.switch-cell',
+		        map: {
+		            on: '1',
+		            off: '0'
+		        },
+		        onTapBefore: function() {
+		            //console.log('before');
+		        },
+		        onTap: function(index) {
+		            console.log('click ali', index);
+		        },
+		        onTapAfter: function() {
+		            //console.log('after');
+		        }
+		    });
+		    var gridItem = new JDUI.instance.GridItem({
+		        title: "",
+		        hook: ".grid-item-wrap",
+		        gridNum: '3',
+		        value: '0',
+		        map: [{
+		            icon: "&#xe603;",
+		            text: "模式1",
+		            value: "0"
+		        }, {
+		            icon: "&#xe603;",
+		            text: "模式2",
+		            value: "1"
+		        }, {
+		            icon: "&#xe603;",
+		            text: "模式3",
+		            value: "2"
+		        }, {
+		            icon: "&#xe603;",
+		            text: "模式4",
+		            value: "3"
+		        }, {
+		            icon: "&#xe603;",
+		            text: "模式5",
+		            value: "4"
+		        }, {
+		            icon: "&#xe603;",
+		            text: "模式6",
+		            value: "5"
+		        }],
+
+		        beforeTap: function() {
+		            console.log("before");
+		        },
+		        onTap: function(item, index, content) {
+		            console.log('下发的值: ' + index);
+		        },
+		        afterTap: function() {
+		            console.log("after");
+
+		        }
+		    });
+		    var stitem = new JDUI.instance.ListItem({
+		        title: "",
+		        hook: ".list-item-settime",
+		        map: [{
+		            icon: "",
+		            title: "定时",
+		            subTitle: '',
+		            rightTitle: ""
+		        }],
+		        beforeTap: function() {
+
+		        },
+		        onTap: function(item, context) {
+		            console.log(item, context);
+		            window.location.href = "settime/index.html";
+		        },
+		        afterTap: function() {
+
+		        }
+		    });
+			JDUI.style.themeColor = '#6bc800';
 			$('.grid-item').append('<span class="edit-name" onClick="editName(this)"><i class="iconfont">&#xe604;</i></span>');
-			var confirm1 = new JDUI.instance.Toast({
+		},
+		render: function() {
+			// App.editNameStatus();
+			// confirm1.openConfirm();
+		},
+		bindEvent: function() {
+
+		},
+		editNameStatus: function(bg) {
+			window.confirm1 = new JDUI.instance.Toast({
 			    hook: '.confirm-twobtn-wrap',
 			    type: 'two',
 			    teplMap: {
 			        title: '修改名称',
-			        content: '<p>可修改自定义名称</p>'
+			        content: '<p>可修改自定义名称</p><input type="text" class="inp" id="update_name" />'
 			    },
 			    letBtnFn: function(e) {
+			    	modeName = $('#update_name').val();
+			    	console.log(modeName);
+			    	$(bg).prev().html(modeName);
 			        confirm1.closeConfirm();
 			    },
 			    rightBtnFn: function(e) {
@@ -24,15 +118,9 @@ define(function(){
 			    }
 			});
 		},
-		render: function() {
-
-		},
-		bindEvent: function() {
-
-		},
 	};
+	App.init();
 	function editName(bg) {
-
+		App.editNameStatus(bg);
+		confirm1.openConfirm();
 	}
-	return App;
-});
